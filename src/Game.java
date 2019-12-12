d39784a2045f4bb41ece9d4764ff33eeb8117052
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2019-12-12 13:59:55 +0100

    tulina is doing everything dont force her

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,7 @@ public class Game {
 	public static void main(String[] args) {
 		
 		//Imlon gonna do this part
-		
+		 // irritating imlo gonna do this as if.
 		
 		JFrame f = new JFrame();// creating instance of JFrame
 		f.setSize(1080, 720);// 400 width and 500 height
