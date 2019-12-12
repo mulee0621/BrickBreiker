7b3cb4436d0b60b78b60cab55a6fce891c51c499
Tulina Maharjan <tulina.maharjan@lt.hs-fulda.de>
2019-12-12 13:15:44 +0100

    change

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 
 public class Game {
 	public static void main(String[] args) {
+		
+		//Imlon gonna do this part
+		
+		
 		JFrame f = new JFrame();// creating instance of JFrame
 
 		JButton b = new JButton("click");// creating instance of JButton
@@ -9,7 +13,7 @@ public class Game {
 
 		f.add(b);// adding button in JFrame
 
-		f.setSize(400, 500);// 400 width and 500 height
+		f.setSize(500, 500);// 400 width and 500 height
 		f.setLayout(null);// using no layout managers
 		f.setVisible(true);// making the frame visible
 	}
