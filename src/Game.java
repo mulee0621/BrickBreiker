5f475c3fa90333bc9d92507d0a98e1966cb46705
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2019-12-28 02:13:23 +0100

    screen with acm framework

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,48 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class Game {
+public class Game extends GraphicsProgram {
+	private static final long serialVersionUID = 1L;
+	protected static boolean GameOver=false;
 	
-	
-	public static void main(String[] args) {
+	//background and size  of screen 
+	@Override
+	public void init() {
+		addKeyListeners();
+		setBackground(Color.black);
+		setSize(700, 600);
+	}
+	@Override
+	public void run() {
+		addWall();
+		addBar();
+		addBall();
+	}
+
+
+	//method creates an object a ball
+	private void addBall() {
 		
+	}
+	//method creates an object a bar
+	private void addBar() {
+		
+	}
+	//method creates an object a wall
+	private void addWall() {
+		
+	}
+	public static void main(String[] args) {
+		new Game().start();
 	}
 }
