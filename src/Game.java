11b26c5157f518c66441dbe1d8835e322c36cae9
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2019-12-28 01:55:05 +0100

    change of framework

--- src/Game.java
+++ src/Game.java
@@ -1,27 +1,8 @@
-import javax.swing.*;
 
 public class Game {
 	
 	
 	public static void main(String[] args) {
 		
-		//Imlon gonna do this part
-		//not Imlon it is Imron
-		 // irritating imlo gonna do this as if.
-		
-		JFrame f = new JFrame();// creating instance of JFrame
-		f.setSize(1080, 720);// 400 width and 500 height
-		JCheckBox cb=new JCheckBox();
-		cb.setBounds(100, 100, 50, 50);
-		f.add(cb);
-		
-		JButton b = new JButton("click");// creating instance of JButton
-		b.setBounds(200, 400, 100, 40);// x axis, y axis, width, height
-
-		f.add(b);// adding button in JFrame
-		
-
-		f.setLayout(null);// using no layout managers
-		f.setVisible(true);// making the frame visible
 	}
 }
