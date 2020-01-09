45e251fa14bf8fb6a8de441df1b4234e60c0e987
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2020-01-09 13:25:30 +0100

    bar speed is changed

--- src/Bar.java
+++ src/Bar.java
@@ -7,7 +7,7 @@ public class Bar extends GRect {
 	private static double posY=500;
 	private static double width=100;
 	private static double height=8;
-	private static double dx=5;
+	private static double dx=25;
 	
 	//this is constructor
 	public Bar() {
