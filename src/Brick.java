2eaae7cb462b4cca453200944fbfb0f3a3386daf
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2020-01-28 18:53:45 +0100

    bricks are commented

--- src/Brick.java
+++ src/Brick.java
@@ -1,7 +1,18 @@
 import java.awt.Color;
 import acm.graphics.*;
-
+/**
+ * This class is Brick and extending Rectangle class
+ * @author Myunguen Lee
+ */
 public class Brick extends GRect{
+	private static final long serialVersionUID = 1L;
+
+/**
+ * 
+ * @param x x-Koordinate von Brick
+ * @param y y-Koordinate von Brick
+ * @param c Farbe von Brick
+ */
 	public Brick(double x, double y, Color c) {
 		super(x, y, 50, 20);
 		setFilled(true);
