6c5754804dcb81655c19df1e04868e27407ebc32
Myung <mulee0621@gmail.com>
2020-01-29 14:05:15 +0100

    Kommentare sind eingestellt.

--- src/Brick.java
+++ src/Brick.java
@@ -1,6 +1,11 @@
 import java.awt.Color;
 import acm.graphics.*;
-
+/**
+ * Diese Klasse behandelt die Darstellung von Blöcken und gibt Eigenschaften
+ * von der x-Startposition, y-Startposition, Größe und Farben ein.
+ * @author mulee
+ *
+ */
 public class Brick extends GRect{
 	public Brick(double x, double y, Color c) {
 		super(x, y, 50, 20);
